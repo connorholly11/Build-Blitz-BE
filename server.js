97b4d4ffff8e54774782a5d4c741a0e4b6b9c700
Connor Holly <connorholly11@gmail.com>
2019-11-13 23:11:11 -0700

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const UsersRouter = require("./data/users/users.router");
 const ProjectsRouter = require("./data/projects/projects.router");
 
 const server = express();
 
+server.use(cors());
+server.use(helmet());
 server.use(express.json());
 server.use("/users", UsersRouter);
 server.use("/projects", ProjectsRouter);
