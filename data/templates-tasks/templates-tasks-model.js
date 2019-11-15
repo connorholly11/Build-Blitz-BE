6f19a676e4f4704901e2726e762f6d7fac4cd502
Connor Holly <connorholly11@gmail.com>
2019-11-14 20:17:03 -0700

    done with endpoints

--- data/templates-tasks/templates-tasks-model.js
+++ data/templates-tasks/templates-tasks-model.js
@@ -1,13 +1,15 @@
 const db = require("../db.config");
 
 module.exports = {
-  getAllTasksAndTemplates,
+  //   getAllTasksAndTemplates,
   getTasksByTemplateId
 };
 
-function getAllTasksAndTemplates() {
-  return db("templates_tasks");
-}
+//NEED A PRIMARY KEY TO RETURN ALL TEMPLATE AND TASK COMBOS
+
+// function getAllTasksAndTemplates() {
+//   return db("templates_tasks");
+// }
 
 function getTasksByTemplateId(template_id) {
   return db("templates_tasks").where("template_id", "=", template_id);
--- data/templates-tasks/templates-tasks.router.js
+++ data/templates-tasks/templates-tasks.router.js
@@ -3,18 +3,20 @@ const db = require("./templates-tasks-model");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  db.getAllTasksAndTemplates()
-    .then(tasks => {
-      res.status(200).json(tasks);
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error,
-        message: "500 server error on getting all tasks and templates"
-      });
-    });
-});
+//NEED A PRIMARY KEY TO RETURN ALL TEMPLATE AND TASK COMBOS
+
+// router.get("/", (req, res) => {
+//   db.getAllTasksAndTemplates()
+//     .then(TasksAndTemplates => {
+//       res.status.json(TasksAndTemplates);
+//     })
+//     .catch(error => {
+//       res.status(500).json({
+//         error: error,
+//         message: "500 server error on getting tasks and templates"
+//       });
+//     });
+// });
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
