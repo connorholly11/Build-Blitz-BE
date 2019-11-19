de73f3ea56e227dc76f8d7c16f275d7b460a49c2
Connor Holly <connorholly11@gmail.com>
2019-11-19 16:04:53 -0700

    changed config

--- data/db.config.js
+++ data/db.config.js
@@ -1,5 +1,14 @@
+// const knex = require("knex");
+
+// const knexConfig = require("../knexfile");
+
+// module.exports = knex(knexConfig.development);
+
 const knex = require("knex");
 
-const knexConfig = require("../knexfile");
+const config = require("../knexfile");
 
-module.exports = knex(knexConfig.development);
+const dbEnv = process.env.DB_ENV || "development";
+const configObj = config[dbEnv];
+
+module.exports = knex(configObj);
--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,6 @@
-// // Update with your config settings.
-
-// module.exports = {
-//   development: {
-//     client: "sqlite3",
-//     connection: {
-//       filename: "./data/information.db3"
-//     },
-//     useNullAsDefault: true,
-
-//     pool: {
-//       afterCreate: (conn, done) => {
-//         conn.run("PRAGMA foreign_keys = ON", done);
-//       }
-//     },
-//     migrations: {
-//       directory: "./data/migrations"
-//     },
-//     seeds: {
-//       directory: "./data/seeds"
-//     }
-//   },
-
-//   production: {
-//     client: "pg",
-//     useNullAsDefault: true,
-
-//     connection: process.env.DATABASE_URL,
-
-//     migrations: {
-//       directory: "./data/migrations",
-//       tableName: "knex_migrations"
-//     },
-
-//     seeds: {
-//       directory: "./data/seeds"
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     }
-//   }
-// };
+// Update with your config settings.
+const productionConnection =
+  process.env.DATABASE_URL || "postgres://localhost/postgres";
 
 module.exports = {
   development: {
@@ -63,35 +23,23 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+    client: "pg",
+    useNullAsDefault: true,
+
+    connection: productionConnection,
+
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations"
+    },
+
+    seeds: {
+      directory: "./data/seeds"
     },
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
     }
   }
 };
